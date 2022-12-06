a62657956cc96e7886ad24f2fad6d788f231d1f2
Aman Ahmed <aman.ahmed97@gmail.com>
2022-12-05 22:42:50 -0500

    src3 code

--- backend/src/main/java/com/example/backend/HasherController.java
+++ backend/src/main/java/com/example/backend/HasherController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.*;
+import java.math.BigInteger;
+import java.net.*;
+import java.security.MessageDigest;
+
 @RestController
 public class HasherController {
 
@@ -14,15 +19,55 @@ public class HasherController {
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/crack")
-	public Dictionary cracker(@RequestParam(value = "hash", defaultValue = "password") String hash) {
+	public Dictionary cracker(@RequestParam(value = "hash", defaultValue = "password") String hash) throws IOException{				
 		
-		// return new Greeting(counter.incrementAndGet(), String.format(template, hash));
-		// String name1 = "greeting";
-		// return name1;
+		String HOSTNAME = "localhost";
+		int PORT = 9090;
+		String password;
+
+		// Connect to server		
+			Socket clientSocket = new Socket(HOSTNAME, PORT);
+
+			InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+			BufferedReader in = new BufferedReader(isr);
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            {
+
+                System.out.println("Connected to " + HOSTNAME + " on port " + PORT);
+                System.out.println("Choose if you want to connect this user for ");
+//            String Mode = in.readLine();
+                String data = "Making First Connection";
+                String SelectMode;
+                System.out.println("Sending to server:\n" + data);
+//                out.println(data);
+//
+                String line;
+////first phase
+//                while ((line = in.readLine()) != null) { //Making tcp Connection
+//                    System.out.println("TCP Connection Made : Client received: " + line);
+//                    break;
+//                }
+//                System.out.println("TCP Connection Successful now waiting For Checking Password."); // When the server replies the first time we move to First Phase for sending the message
+////HASH REQUEST
+                //second phase
+                password = "ABCDE";
+                // String Hash = getHashValue(password);
+                out.println(hash);
+
+                while (!(line = in.readLine()).equals(password)) { //Making tcp Connection
+
+
+                }
+                System.out.println("Password for the Following Hash is : " + line);
+
+
+        }        
+
 
 		Dictionary response = new Hashtable();
 		response.put("hash", hash);
-		response.put("password", "password-cracked");
+		response.put("password", password);
 
 		return response;
 	}
